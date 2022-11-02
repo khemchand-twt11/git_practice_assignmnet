b109d1b1b3d9c5c63428667fce72ddea97312cd7
khemchand-twt11 <gauravsharma6290852476@gmail.com>
2022-11-02 18:04:06 +0530

    Check prime number code

---
+++ prime.js
@@ -0,0 +1,20 @@
+function primeCheck(num) {
+  let count = 0;
+  for (let i = 0; i <= num; i++) {
+    if (num % i == 0) {
+      count++;
+    }
+    if (count == 2) {
+      return true;
+    } else {
+      return false;
+    }
+  }
+}
+
+let ans = primeCheck(13);
+if (ans) {
+  console.log("Prime");
+} else {
+  console.log("Not a Prime");
+}
